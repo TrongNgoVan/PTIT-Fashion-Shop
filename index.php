2d3c9c8964db75e1bb5c7b1ba16545e0470e9278
Ngọ Văn Trọng <ngovantrong1308@gmail.com>
2025-02-24 02:56:04 +0700

    thêm 1 số hình ảnh thời trangtrang

--- index.php
+++ index.php
@@ -79,7 +79,7 @@ require_once('components/header.php');
             <div class="row">
                 <div class="col-lg-12">
                     <div class="section-title">
-                        <h2>Sản phẩm đặc trưng</h2>
+                        <h2>Các mẫu đặc trưng</h2>
                     </div>
                     <div class="featured__controls">
                         <ul>
@@ -159,7 +159,7 @@ require_once('components/header.php');
             <div class="row">
                 <div class="col-lg-4 col-md-6">
                     <div class="latest-product__text">
-                        <h4>Latest Products</h4>
+                        <h4>Latest Fashion</h4>
                         <div class="latest-product__slider owl-carousel">
                             <div class="latest-prdouct__slider__item">
                                 <a href="#" class="latest-product__item">
@@ -224,7 +224,7 @@ require_once('components/header.php');
                 </div>
                 <div class="col-lg-4 col-md-6">
                     <div class="latest-product__text">
-                        <h4>Top Rated Products</h4>
+                        <h4>Top Rated Fashion</h4>
                         <div class="latest-product__slider owl-carousel">
                             <div class="latest-prdouct__slider__item">
                                 <a href="#" class="latest-product__item">
@@ -289,7 +289,7 @@ require_once('components/header.php');
                 </div>
                 <div class="col-lg-4 col-md-6">
                     <div class="latest-product__text">
-                        <h4>Review Products</h4>
+                        <h4>Review Fashion</h4>
                         <div class="latest-product__slider owl-carousel">
                             <div class="latest-prdouct__slider__item">
                                 <a href="#" class="latest-product__item">
